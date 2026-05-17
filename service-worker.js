4091d9fc5f41e9ba4028724b50794c029e5fa98d
davidbartolialabro <david.bartoli.alabro@gmail.com>
2026-05-17 21:02:11 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "inclinacio-pedra-v2";
+const CACHE_NAME = "inclinacio-pedra-v3";
 
 const FILES_TO_CACHE = [
   "index.html",
@@ -8,11 +8,27 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
